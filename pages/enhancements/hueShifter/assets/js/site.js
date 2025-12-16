1c33c7fcc011bea79facb336e72d54cc3aae7512
bo-nicolaisen <bni@techcollege.dk>
2025-12-16 13:25:46 +0100

    #8
    created branch enhancements and added my hue shifter page

--- pages/enhancements/hueShifter/assets/js/site.js
+++ pages/enhancements/hueShifter/assets/js/site.js
@@ -1,15 +1,28 @@
-// write cool JS hwere!!
-let deg=0;
+/* global variables */
+
+let deg=0; //deg 0-360
 const speed=1; //ms
 const step=10; //deg
 let autoMode=true;
 
-let autoUpfdate=null;
-let myTimeout=false;
+let autoUpdate=null; /*Interval variable*/
+let myTimeout=false; /*Timeout variable*/
+
+/*Get HTML elements*/
 
 let myShifterElement=document.getElementById("shifter");
 let myHueSlider=document.getElementById("hueSlider");
 let autoHueButton=document.getElementById("autoHue");
+
+
+/*Event Listeners*/
+myHueSlider.addEventListener("input", (event) => {
+     deg=event.target.value;
+
+     clearInterval(autoUpdate);
+    updateHue();
+
+});
 
 
 autoHueButton.addEventListener("click", (event) => {
@@ -17,7 +30,7 @@ autoHueButton.addEventListener("click", (event) => {
     if(autoMode){
         autoMode=false;
         autoHueButton.textContent="start";
-        clearInterval(autoUpfdate);
+        clearInterval(autoUpdate);
     }else{
         autoMode=true;
         autoHueButton.textContent="stop";
@@ -28,30 +41,22 @@ autoHueButton.addEventListener("click", (event) => {
    
 });
 
+
+
+/*Start automatic Hue Shift*/
 automateHue()
 
 
+/*Automate Hue Shift by interval and step*/
 function automateHue(){
-
- autoUpfdate=setInterval(() => {
-    deg+=step%360;
-      myHueSlider.value = deg%360;
+ autoUpdate=setInterval(() => {
+    deg=(deg+step)%360;
+      myHueSlider.value = deg;
    updateHue();
 }, 100);
-
 }
 
-
-console.log("update");
-
-myHueSlider.addEventListener("input", (event) => {
-     deg=event.target.value;
-
-     clearInterval(autoUpfdate);
-    updateHue();
-
-});
-
+/*Update Hue Shift filter*/
 
 function updateHue(){
     myShifterElement.style.filter = `hue-rotate(${deg}deg)`;
